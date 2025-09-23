d3bd6f4ca8a05a081ea77cee6c617dacb2aa08cd
Tico1606 <gabrielsoliveira1606@gmail.com>
2025-09-22 21:04:37 -0300

    ♻️ refactor: remove unused handlers and simplify Home, Profile, Login, and SignUp components

--- src/app/(auth)/home.tsx
+++ src/app/(auth)/home.tsx
@@ -1,50 +1,5 @@
 import { HomePage } from '@/components'
 
 export default function Home() {
-  const handleSearchPress = () => {
-    // TODO: Implementar navegação para tela de busca
-    console.log('Search pressed')
-  }
-
-  const handleFilterPress = () => {
-    // TODO: Implementar filtros
-    console.log('Filter pressed')
-  }
-
-  const handleNotificationPress = () => {
-    // TODO: Implementar notificações
-    console.log('Notification pressed')
-  }
-
-  const handleCategoryPress = (category: any) => {
-    // TODO: Implementar navegação para categoria
-    console.log('Category pressed:', category)
-  }
-
-  const handleRecipePress = (recipe: any) => {
-    // TODO: Implementar navegação para receita
-    console.log('Recipe pressed:', recipe)
-  }
-
-  const handleViewAllRecipes = () => {
-    // TODO: Implementar navegação para todas as receitas
-    console.log('View all recipes pressed')
-  }
-
-  const handleRecipeLike = (recipe: any) => {
-    // TODO: Implementar funcionalidade de curtir
-    console.log('Recipe liked:', recipe)
-  }
-
-  return (
-    <HomePage
-      onSearchPress={handleSearchPress}
-      onFilterPress={handleFilterPress}
-      onNotificationPress={handleNotificationPress}
-      onCategoryPress={handleCategoryPress}
-      onRecipePress={handleRecipePress}
-      onViewAllRecipes={handleViewAllRecipes}
-      onRecipeLike={handleRecipeLike}
-    />
-  )
+  return <HomePage />
 }
--- src/app/(auth)/profile.tsx
+++ src/app/(auth)/profile.tsx
@@ -1,40 +1,5 @@
-import { useAuth, useUser } from '@clerk/clerk-expo'
-import { useRouter } from 'expo-router'
-import { Alert } from 'react-native'
-
 import { ProfilePage } from '@/components'
 
 export default function Profile() {
-  const { signOut } = useAuth()
-  const { user } = useUser()
-  const router = useRouter()
-
-  const handleSignOut = async () => {
-    try {
-      await signOut()
-      router.replace('/(public)/login' as any)
-    } catch (error) {
-      console.error('Erro ao fazer logout:', error)
-      Alert.alert('Erro', 'Não foi possível fazer logout')
-    }
-  }
-
-  const handleSettingsPress = () => {
-    // TODO: Implementar navegação para configurações
-    console.log('Settings pressed')
-  }
-
-  const handleMenuItemPress = (item: any) => {
-    // TODO: Implementar navegação para item do menu
-    console.log('Menu item pressed:', item)
-  }
-
-  return (
-    <ProfilePage
-      user={user}
-      onSettingsPress={handleSettingsPress}
-      onMenuItemPress={handleMenuItemPress}
-      onSignOut={handleSignOut}
-    />
-  )
+  return <ProfilePage />
 }
